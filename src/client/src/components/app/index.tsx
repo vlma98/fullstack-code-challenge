c7560d1047230693e013f0c1e10359f5b5b88556
Vitor Maia <vlma@cin.ufpe.br>
2020-09-27 19:35:16 -0300

    add contexts to component tree

--- src/client/src/components/app/index.tsx
+++ src/client/src/components/app/index.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import { Redirect, Router } from '@reach/router'
 import './app.less'
 import { Auth, Dashboard } from '../pages'
-
+import { UserContextProvider } from '../../contexts/users'
+import { RepoContextProvider } from '../../contexts/repos'
 function App() {
   return (
     <div className='App'>
-      <Router>
-        <Redirect noThrow from='/' to='/auth' />
-        <Auth path='/auth' />
-        <Dashboard path='/dashboard' />
-      </Router>
+      <UserContextProvider>
+        <RepoContextProvider>
+          <Router>
+            <Redirect noThrow from='/' to='/auth' />
+            <Auth path='/auth' />
+            <Dashboard path='/dashboard' />
+          </Router>
+        </RepoContextProvider>
+      </UserContextProvider>
     </div>
   )
 }

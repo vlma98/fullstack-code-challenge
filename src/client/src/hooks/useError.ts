72825f3dab460f30b0e8a39d17f9bffd70be4c17
Vitor Maia <vlma@cin.ufpe.br>
2020-09-29 11:02:11 -0300

    smaller duration time

--- src/client/src/hooks/useError.ts
+++ src/client/src/hooks/useError.ts
@@ -1,14 +1,14 @@
 import { notification } from 'antd'
 import { useState, useEffect } from 'react'
 
-const DURATION = 5000
+const DURATION = 3000
 
 const useError = function (): [string, (error: string) => void] {
   const [error, setError] = useState('')
 
   useEffect(() => {
     if (error) {
-      notification.open({ type: 'error', message: error })
+      notification.open({ type: 'error', message: error, duration: DURATION })
       setTimeout(() => {
         setError('')
       }, DURATION)
--- src/client/src/hooks/useSuccess.ts
+++ src/client/src/hooks/useSuccess.ts
@@ -1,14 +1,18 @@
 import { notification } from 'antd'
 import { useState, useEffect } from 'react'
 
-const DURATION = 5000
+const DURATION = 3000
 
 const useSuccess = function (): [string, (error: string) => void] {
   const [success, setSuccess] = useState('')
 
   useEffect(() => {
     if (success) {
-      notification.open({ type: 'success', message: success })
+      notification.open({
+        type: 'success',
+        message: success,
+        duration: DURATION
+      })
       setTimeout(() => {
         setSuccess('')
       }, DURATION)
